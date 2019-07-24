36a660b2e42d7994beae5646e28c1d23a0a5a1c7
dinocajic <dinocajic@gmail.com>
2019-07-24 09:43:58 -0400

    Initial Commit

---
+++ app/src/main/java/edu/gsu/student/wheels/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.gsu.student.wheels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
