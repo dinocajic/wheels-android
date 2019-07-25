8916aeaa11856c25e260fe7e428f77b486fb206e
dinocajic <dinocajic@gmail.com>
2019-07-25 08:46:02 -0400

    Finished creating Help Screen with 2 fragments

--- app/src/main/java/edu/gsu/student/wheels/WheelsActivity.java
+++ app/src/main/java/edu/gsu/student/wheels/WheelsActivity.java
@@ -2,12 +2,17 @@ package edu.gsu.student.wheels;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class WheelsActivity extends AppCompatActivity {
 
